68b2a4f511a706fb9fd347be9789870140072b59
gustavoabarros <gustavo.barros@outlook.com>
2015-01-28 14:30:11 -0200

    Asset adding

---
+++ Spread.Portfolio.Library/Models/Asset.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Spread.Portfolio.Library.Models
+{
+    public class Asset
+    {
+    
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        public int IdAsset { get; set; }
+
+        public int IdCategory { get; set; }
+
+        public string NmAsset { get; set; }
+
+        public string CodeISIN { get; set; }
+
+        public string CodeSELIC { get; set; }
+
+        public DateTime MaturityDate { get; set; }
+
+        public int MaturityExpectedInDays { get; set; }
+
+        public decimal GraceDate { get; set; }
+
+        public decimal GraceExpectedInDays { get; set; }
+    }
+}
